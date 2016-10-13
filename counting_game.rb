e0cccc74ae67b5e2f4798100b21f5182a743f44d
Sampson R Crowley <sampsonsprojects@gmail.com>
2016-10-13 16:43:47 -0600

    final commit

--- counting_game.rb
+++ counting_game.rb
@@ -1,25 +1,29 @@
 
 def counting_game (players,max)
-  player = 1
-  count = 0
+  player = 0
   direction = 1
   (1..max).each{|count|
-    if (count % 7 == 0)
-      direction *= -1
-    end
-    if count % 11 == 0
-      change = 2
+    if !(count == 1)
+      if ((count-1).abs % 7 == 0)
+        direction *= -1
+      end
+      if (0 == (count-1) % 11 )
+        change = 2
+      else
+        change = 1
+      end
     else
-      change = 1
+        direction = 1
+        change = 1
     end
     player = player + (change * direction)
     if !player.between?(1,players)
       player = (player.abs - players).abs
     end
-    #puts "#{player} player, #{count} count, #{direction} direction, #{count % 7 == 0} 7, #{change} change, #{count % 11 == 0} 11"
+    #puts "player: #{player}, count: #{count}, direction: #{direction}, divisible by 7  #{(count-1) % 7 == 0}, change: #{change}, divisible by 11: #{(count-1) % 11 == 0}"
   }
   return player
 end
 
-puts counting_game(3,100)
+puts counting_game(10,100)
 puts counting_game(60,100)
--- project_euler_problems.rb
+++ project_euler_problems.rb
@@ -57,7 +57,133 @@ def largest_palindrome(number_of_digits,symetrical=true)
   }
 end
 
+def smallest_multiple(low,high)
+  divisible = false
+  number = low * high
+  while !divisible
+    divisible = true
+    number = number + high
+    (low..high).each{|i|
+      if !(number % i == 0)
+        divisible = false
+        break
+      end
+    }
+  end
+  return number
+end
+
+def sum_square_difference(range)
+  sum = 0
+  square = 0
+  (range).each{|number|
+    sum += number
+    square += number**2
+  }
+  difference = (sum**2 - square).abs
+end
+
+
+def nth_prime(target, n = false)
+  if !n
+    n = target**2
+  end
+  nums = [nil, nil, *2..n]
+  (2..Math.sqrt(n)).each do |i|
+    (i**2..n).step(i){|m| nums[m] = nil}  if nums[i]
+  end
+  primes = nums.compact
+  if primes.length < target
+    return nth_prime(target, n**2)
+  end
+  return primes[target-1]
+end
+
+def largest_product_in_a_series(number_string,step)
+  @record = 0
+  @position = 0
+  numbers = number_string.split("")
+  num_list = []
+  numbers.each{|number| num_list.push(number.to_i)}
+  iterate = num_list.length-step
+  iterate.times do |i|
+    @sum = 1
+    (i..i+step-1).step(1){|m| 
+      @sum *= num_list[m]
+    }
+    if @sum > @record
+      @position = i
+      @record = @sum
+    end
+  end
+  return "position: #{@position}, total: #{@record}"
+end
+
+def summation_of_primes(max)
+  n = max
+  nums = [nil, nil, *2..n]
+  (2..Math.sqrt(n)).each do |i|
+    (i**2..n).step(i){|m| nums[m] = nil}  if nums[i]
+  end
+  primes = nums.compact
+  return primes.inject(:+)
+end
+
+def reverse(input_array)
+  while input_array.length != 0 
+    second.push(input_array[-1])
+    input_array.pop
+  end
+  return second
+end
+
+def light_switches
+  @nums = []
+  (1..100).each{
+    @nums.push("off")
+  }
+  (1..100).each{|trip|
+    (trip-1..99).step(trip){|m|
+      if @nums[m] == "off"
+        @nums[m] = "on"
+      else
+        @nums[m] = "off"
+      end
+    }
+  }
+  return @nums
+end
+
+def stock_picker(prices)
+  @largest = 0
+  @pair = [0,0]
+  i=0
+  while i < prices.length-1
+    n = i+1
+    while n < prices.length
+      difference = prices[n] - prices[i]
+      if difference > @largest
+        @largest = difference
+        @pair = [i,n]
+      end
+      n += 1
+    end
+    i += 1
+  end
+  return @pair
+end
+
+
+
 puts three_and_five(1..1000)
 puts fibonacci_sum(4000000)
 puts largest_prime(600851475143)
 puts largest_palindrome(3)
+puts smallest_multiple(1,10)
+puts sum_square_difference(1..100)
+puts nth_prime(6)
+puts nth_prime(100)
+puts largest_product_in_a_series("09458394323523452345234523452345234523452345234523459303948",13)
+puts summation_of_primes(30)
+p light_switches
+p stock_picker([44,10,66,73,12,12,54,23,67,87,1,1000])
